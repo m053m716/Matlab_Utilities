73613d131d85ad10da2c41d083627d0ecc1ce066
Max-Home-Tower <max.murphy11@gmail.com>
2020-06-29 18:06:14 -0500

    Add adapted RainCloudPlots code
    fix

--- +gfx__/Contents.m
+++ +gfx__/Contents.m
@@ -1,6 +1,9 @@
 % GFX__ Package containing utilities for making nicer MATLAB graphics
 % MATLAB Version 9.7 (R2019b Update 5) 02-Jun-2020
 %
+% Sub-packages
+%	rain                - Contains adapted RainCloudPlots code
+%
 % Files
 %   addSignificanceLine - Add line to axes indicating significant epoch(s)
 %   addToSecondMonitor  - Return position coordinates for 2nd mon if possible
